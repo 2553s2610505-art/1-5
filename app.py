c43319880b3b20de3fc8c10d789ab298001c28ec
2553s2610505-art <2553s2610505@g.cnees.kr>
2026-05-29 13:22:34 +0900

    app.py

--- app.py
+++ app.py
@@ -4,3 +4,9 @@ st.set_page_config(page_title="My App", page_icon="🚀")
 
 st.title("첫 번째 스트림릿 앱")
 st.write("GitHub + Streamlit 배포 성공!")
+git init
+git add .
+git commit -m "first app"
+git branch -M main
+git remote add origin https://github.com/아이디/레포이름.git
+git push -u origin main
